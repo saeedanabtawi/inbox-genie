0850a02e3c41ed90cf4b6f08eca5f88362b33ca3
Saeed Anabtawi <saeedanabtawi@users.noreply.github.com>
2025-04-01 03:20:51 +0300

    store to the database

--- src/models.py
+++ src/models.py
@@ -42,105 +42,16 @@ class User(UserMixin, db.Model):
         """Required by Flask-Login"""
         return str(self.id)
     
-    # In-memory subscription data (not stored in database)
-    # This is a temporary development solution
-    _subscription_data = {}
-    
-    def _get_subscription_data(self, key, default):
-        """Get subscription data from in-memory storage"""
-        user_data = self._subscription_data.get(self.id, {})
-        return user_data.get(key, default)
-    
-    def _set_subscription_data(self, key, value):
-        """Set subscription data in in-memory storage"""
-        if self.id not in self._subscription_data:
-            self._subscription_data[self.id] = {}
-        self._subscription_data[self.id][key] = value
-    
-    # Subscription tier property
-    @property
-    def subscription_tier(self):
-        """Get user's subscription tier"""
-        return self._get_subscription_data('tier', 'Free')
-    
-    @subscription_tier.setter
-    def subscription_tier(self, value):
-        """Set user's subscription tier"""
-        self._set_subscription_data('tier', value)
-    
-    # Subscription dates
-    @property
-    def subscription_start_date(self):
-        """Get subscription start date"""
-        return self._get_subscription_data('start_date', None)
-    
-    @subscription_start_date.setter
-    def subscription_start_date(self, value):
-        """Set subscription start date"""
-        self._set_subscription_data('start_date', value)
-    
-    @property
-    def subscription_end_date(self):
-        """Get subscription end date"""
-        return self._get_subscription_data('end_date', None)
-    
-    @subscription_end_date.setter
-    def subscription_end_date(self, value):
-        """Set subscription end date"""
-        self._set_subscription_data('end_date', value)
-    
-    # Billing cycle
-    @property
-    def is_annual_billing(self):
-        """Get annual billing status"""
-        return self._get_subscription_data('annual_billing', False)
-    
-    @is_annual_billing.setter
-    def is_annual_billing(self, value):
-        """Set annual billing status"""
-        self._set_subscription_data('annual_billing', value)
-    
-    # Usage metrics
-    @property
-    def emails_generated(self):
-        """Get emails generated count"""
-        return self._get_subscription_data('emails_generated', 0)
-    
-    @emails_generated.setter
-    def emails_generated(self, value):
-        """Set emails generated count"""
-        self._set_subscription_data('emails_generated', value)
-    
-    @property
-    def emails_sent(self):
-        """Get emails sent count"""
-        return self._get_subscription_data('emails_sent', 0)
-    
-    @emails_sent.setter
-    def emails_sent(self, value):
-        """Set emails sent count"""
-        self._set_subscription_data('emails_sent', value)
-    
-    @property
-    def bulk_campaigns(self):
-        """Get bulk campaigns count"""
-        return self._get_subscription_data('bulk_campaigns', 0)
-    
-    @bulk_campaigns.setter
-    def bulk_campaigns(self, value):
-        """Set bulk campaigns count"""
-        self._set_subscription_data('bulk_campaigns', value)
-    
-    @property
-    def current_month_usage(self):
-        """Get current month usage"""
-        return self._get_subscription_data('current_month_usage', 0)
-    
-    @current_month_usage.setter
-    def current_month_usage(self, value):
-        """Set current month usage"""
-        self._set_subscription_data('current_month_usage', value)
-    
+    # Database fields for subscription data
+    subscription_tier = db.Column(db.String(20), default='Free')
+    subscription_start_date = db.Column(db.DateTime, nullable=True)
+    subscription_end_date = db.Column(db.DateTime, nullable=True)
+    is_annual_billing = db.Column(db.Boolean, default=False)
+    emails_generated = db.Column(db.Integer, default=0)
+    emails_sent = db.Column(db.Integer, default=0)
+    bulk_campaigns = db.Column(db.Integer, default=0)
+    current_month_usage = db.Column(db.Integer, default=0)
+
     def get_monthly_email_limit(self):
         """Get the monthly email limit based on subscription tier"""
         limits = {
@@ -173,16 +84,18 @@ class User(UserMixin, db.Model):
     def reset_monthly_usage(self):
         """Reset the monthly usage counter"""
         self.current_month_usage = 0
+        db.session.commit()
     
     def increment_usage(self, count=1):
         """Increment the usage counters"""
-        self.emails_generated = self.emails_generated + count
-        self.current_month_usage = self.current_month_usage + count
+        self.emails_generated += count
+        self.current_month_usage += count
+        db.session.commit()
     
     def record_bulk_campaign(self, email_count):
         """Record a bulk campaign and its usage"""
-        self.bulk_campaigns = self.bulk_campaigns + 1
-        self.emails_sent = self.emails_sent + email_count
+        self.bulk_campaigns += 1
+        self.emails_sent += email_count
         self.increment_usage(email_count)
     
     def __repr__(self):
